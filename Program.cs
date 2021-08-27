1919531240ffdacb0de28b31b257470379162ab6
Dat <datndd2604@gmail.com>
2021-08-27 21:17:10 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dependency_injection_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
